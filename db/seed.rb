5f8bb8daef8970e9c9476316c53d5ff189220d35
Zakk <zachary.fleischmann@gmail.com>
2015-03-31 16:22:36 -0400

    run bundle install, seed databse

--- db/seed.rb
+++ db/seed.rb
@@ -1,22 +1,22 @@
-relative_require '../app'
-relative_require 'connection'
+require_relative '../app'
+require_relative 'connection'
 
 decks = Deck.create([
-  {name: "Spanish Foods", score: 0}
+  {name: "Spanish Foods", score: 0},
   {name: "Spanish Travel", score: 0}
   ])
 
-spanish_foods_deck = Decks.find_by(name: "Spanish Foods")
-spanish_foods_cards = spanish_foods_deck.card.create([
-  {front: "Pina", back: "Pineapple", deck_id: spanish_foods_deck.id}
-  {front: "Manzana", back: "Apple", deck_id: spanish_foods_deck.id}
+spanish_foods_deck = Deck.find_by(name: "Spanish Foods")
+spanish_foods_cards = spanish_foods_deck.cards.create([
+  {front: "Pina", back: "Pineapple", deck_id: spanish_foods_deck.id},
+  {front: "Manzana", back: "Apple", deck_id: spanish_foods_deck.id},
   {front: "Aguacate", back: "Avocado", deck_id: spanish_foods_deck.id}
   ])
 
-spanish_travel_deck = Decks.find_by(name: "Spanish Travel")
-spanish_travel_cards = spanish_travel_deck.card.create([
-  {front: "Autobus", back: "Bus", deck_id: spanish_foods_deck.id}
-  {front: "Auto", back: "Car", deck_id: spanish_foods_deck.id}
+spanish_travel_deck = Deck.find_by(name: "Spanish Travel")
+spanish_travel_cards = spanish_travel_deck.cards.create([
+  {front: "Autobus", back: "Bus", deck_id: spanish_foods_deck.id},
+  {front: "Auto", back: "Car", deck_id: spanish_foods_deck.id},
   {front: "Avion", back: "Airplane", deck_id: spanish_foods_deck.id}
   ])
 
